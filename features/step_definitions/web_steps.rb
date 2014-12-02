446b3146068bec6346eb5cc6c713ff6ce2f563e4
Thomas Ochman <thomas@nocebo.se>
2014-12-02 23:17:16 +0100

    refactored 'web_steps.rb'

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -16,7 +16,7 @@ Given /^I am logged into the admin account$/ do
   fill_in 'user_password', :with => 'catscats'
   click_button 'Login'
   if page.respond_to? :should
-    page.should have_content('Login Successful')
+    expect(page).to have_content 'Login Successful' 
   else
     assert page.has_content?('Login Successful')
   end
