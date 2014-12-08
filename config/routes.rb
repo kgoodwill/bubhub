75ef0f1c0f58fc8796044fbcba4cae315be601ef
htinlinn <htinlinnkyaw@gmail.com>
2014-12-08 18:23:10 -0500

    moved join bike share page to the directory under welcome

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root :to => 'welcome#index'
-  
-  get 'join' => 'join#index'
-
+  get 'join' => 'welcome#join'  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
