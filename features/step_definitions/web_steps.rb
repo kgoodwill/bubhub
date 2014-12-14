7ca5b63206b822c927f32e94e9445937be1b6f2d
daveinthecave <daveman0331@gmail.com>
2014-12-14 17:18:02 -0400

    Added Cucumber Tests

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -22,6 +22,73 @@ Given /^I am logged into the admin account$/ do
   end
 end
 
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit (page_name)
+end
+
+When /^(?:|I )go to (.+)$/ do |page_name|
+  visit (page_name)
+end
+
 When /^(?:|I )press "([^"]*)"$/ do |button|
   click_button(button)
+end
+
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+
+When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
+  select(value, :from => field)
+end
+
+When /^(?:|I )check "([^"]*)"$/ do |field|
+  check(field)
+end
+
+When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+  uncheck(field)
+end
+
+When /^(?:|I )choose "([^"]*)"$/ do |field|
+  choose(field)
+end
+
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+
+Then /^the "([^"]*)" field(?: within (.*))? should contain "([^"]*)"$/ do |field, parent, value|
+  with_scope(parent) do
+    field = find_field(field)
+    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    if field_value.respond_to? :should
+      field_value.should =~ /#{value}/
+    else
+      assert_match(/#{value}/, field_value)
+    end
+  end
+end
+
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
+
+
+Then /^show me the page$/ do
+  save_and_open_page
 end
