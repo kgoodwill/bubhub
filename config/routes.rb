746fb0754e79b342115e35f1648607d1e954cdbd
Kyle Goodwill <k.kyle.goodwill@gmail.com>
2014-12-16 12:26:29 -0500

    added link for the how it works page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/contact_us", to: "welcome#contact_us", as: :contact_us_page  
   get '/resources', to: 'welcome#resources', as: :resources_page
   get '/bike_shop_info', to: 'welcome#bike_shop_info', as: :bike_shop_info_page
+  get '/how_it_works', to: "welcome#how_it_works", as: :how_it_works_page
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
